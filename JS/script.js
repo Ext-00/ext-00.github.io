6c6f78b8ca463cd2822c5b72d7d299710922c7a9
Ext-00 <114066149+Ext-00@users.noreply.github.com>
2023-01-10 15:10:12 +0530

    v2.7

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,14 @@
-const toggleButton = document.getElementsByClassName('toggle-button')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+//const toggleButton = document.getElementsByClassName('toggle-button')[0]
+//const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-toggleButton.addEventListener('click', () => {
-  navbarLinks.classList.toggle('active')
-})
+//toggleButton.addEventListener('click', () => {
+//  navbarLinks.classList.toggle('active')
+//})
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
